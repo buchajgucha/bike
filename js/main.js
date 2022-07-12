a61fc720c476b63111bb8d774b49d6adc94e2db6
buchajgucha <sportnashevse@yandex.ru>
2022-07-12 18:43:02 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function () {
+  $(".swiper-slide").slick({
+    arrows: false,
+    dots: true,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
+});
+
+$(function () {
+  $(".swiper_slide-bottom").slick({
+    arrows: false,
+    dots: true,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
+});
